3c2d938917e8d3228cd43d4345cd5444e5719ab8
Damir021 <k.damir021@gmail.com>
2022-11-16 16:42:42 +0600

    add scrollToElement script

---
+++ markup/components/company/company.js
@@ -0,0 +1,15 @@
+function scrollToElement(selector) {
+    const targetElement = document.querySelector(selector)
+    if(!targetElement) return;
+    targetElement.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
+
+const anchorLinksElements = document.querySelectorAll('[data-scroll-to]');
+anchorLinksElements.forEach(link => {
+    const selector = link.dataset.scrollTo;
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        scrollToElement(selector);
+    });
+})
