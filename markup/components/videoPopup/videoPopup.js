897f7c6854e3fdc2c8d488546f74bdfbf75a35ba
Damir021 <k.damir021@gmail.com>
2022-11-16 17:29:34 +0600

    add video popup script

---
+++ markup/components/videoPopup/videoPopup.js
@@ -0,0 +1,46 @@
+//Скрипт для видео плеера
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.insertAdjacentHTML('beforeend', 
+        `<div class="videopopup">
+            <div class="videopopup__close"></div>
+            <div class="videopopup__overlay"></div>
+            <div class="videopopup__container"></div>
+        </div>`
+    );
+    const closeButton = document.querySelector('.videopopup__close');
+    const overlay = document.querySelector('.videopopup__overlay');
+    closeButton.addEventListener('click', videoHide);
+    overlay.addEventListener('click', videoHide);
+});
+
+const videoButtons = document.querySelectorAll('[data-youtubevideo-play]');
+
+videoButtons.forEach(videoButton => {
+    const source = videoButton.dataset.youtubevideoPlay;
+    videoButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        videoShow(source)
+    });
+});
+
+function videoShow(source){
+    const container = document.querySelector('.videopopup__container');
+    const videopopup = document.querySelector('.videopopup');
+    const iframe = `<iframe width="560" height="315" 
+    src="${source}" 
+    title="YouTube video player" frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; 
+    encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>`;
+    container.insertAdjacentHTML('beforeend', iframe);
+    videopopup.classList.add('active');
+    document.body.classList.add('no-scroll');
+}
+function videoHide(){
+    const container = document.querySelector('.videopopup__container');
+    const videopopup = document.querySelector('.videopopup');
+    container.innerHTML = "";
+    videopopup.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+}
+
